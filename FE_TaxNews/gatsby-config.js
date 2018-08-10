4305d231fee539fb65fa535aa6e563b6a31a1a51
yashaswi maharshi <maha.yash@gmail.com>
2018-08-10 18:04:03 +0530

    Get the data from mongo db and displayed it on the page

--- FE_TaxNews/gatsby-config.js
+++ FE_TaxNews/gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-mongodb`,
+      options: {
+        dbName: 'taxnewsbackend',
+        collection: [`topstories`],
+        server: { address: `localhost`, port: `27017` },
+      },
+    },
+  ],
 }
---
+++ FE_TaxNews/src/pages/topStories.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+import Layout from '../layouts'
+
+export default ({ data }) => {
+  let dataArr = data.allMongodbTaxnewsbackendTopstories.edges
+  console.log(dataArr)
+  return (
+    <div>
+      <h1>Top stories</h1>
+      <p>Success</p>
+      <div>
+        <table>
+          <tbody>
+            {dataArr.map(({ node }, index) => (
+              <tr key={index}>
+                <td>
+                  <h2>{node.title}</h2>
+                  <h4>{node.subTitle}</h4>
+                  <a href={node.link} target="_blank">
+                    For more information
+                  </a>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
+  )
+}
+
+//export default topStories
+export const topStoriesQuery = graphql`
+  query topStoriesResult {
+    allMongodbTaxnewsbackendTopstories {
+      edges {
+        node {
+          title
+          subTitle
+          link
+        }
+      }
+    }
+  }
+`
